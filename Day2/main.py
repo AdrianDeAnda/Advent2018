c15d0f02a78b6c09ba3e31ff48867087c9e0b324
Adrian De Anda <adrian.deanda@outlook.com>
2018-12-02 01:04:21 -0600

    Linting Day 2 solution with black

--- Day2/main.py
+++ Day2/main.py
@@ -7,7 +7,7 @@ with open("input.txt") as n:
 
 
 def checksum(ids, twos: int = 0, threes: int = 0):
-    for n in range (len(ids)):
+    for n in range(len(ids)):
         dict = Counter(ids[n])
         if 2 in dict.values():
             twos += 1
@@ -16,14 +16,12 @@ def checksum(ids, twos: int = 0, threes: int = 0):
     return twos * threes
 
 
-assert checksum([
-    "abcdef",
-    "bababc",
-    "abbcde",
-    "abcccd",
-    "aabcdd",
-    "abcdee",
-    "ababab"]) == 12
+assert (
+    checksum(
+        ["abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab"]
+    )
+    == 12
+)
 
 print(f"The checksum is: {checksum(input)}")
 
@@ -33,21 +31,18 @@ print(f"The checksum is: {checksum(input)}")
 def common_letters(ids):
     common_dict = Counter()
     for box_id in ids:
-        for n in range (len(box_id)):
-            delete_one = tuple(box_id[:n] + "_" + box_id[(n+1):])
-            common_dict[delete_one] +=1
+        for n in range(len(box_id)):
+            delete_one = tuple(box_id[:n] + "_" + box_id[(n + 1) :])
+            common_dict[delete_one] += 1
     [(top, count), (bot, bot_count)] = common_dict.most_common(2)
     return "".join([c for c in top if c != "_"])
 
 
-assert common_letters([
-    "abcde",
-    "fghij",
-    "klmno",
-    "pqrst",
-    "fguij",
-    "axcye",
-    "wvxyz"
-    ]) == "fgij"
+assert (
+    common_letters(
+        ["abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz"]
+    )
+    == "fgij"
+)
 
 print(f"The common letters are: {common_letters(input)}")
