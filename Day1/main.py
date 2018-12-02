9ed7093b565e93cafd45edcea2aa248cbdd24985
Adrian De Anda <adrian.deanda@outlook.com>
2018-12-02 01:15:25 -0600

    Typo on second puzzle first assertion test

--- Day1/main.py
+++ Day1/main.py
@@ -29,7 +29,7 @@ def repeated_frequency(numbers, value: int = 0):
         seen.add(value)
 
 
-assert repeated_frequency({+1, -1}) == 0
+assert repeated_frequency([+1, -1]) == 0
 assert repeated_frequency([+3, +3, +4, -2, -4]) == 10
 assert repeated_frequency([-6, +3, +8, +5, -6]) == 5
 assert repeated_frequency([+7, +7, -2, -7, -4]) == 14
