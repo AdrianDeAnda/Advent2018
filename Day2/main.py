ff87bcdecdc4f1ad8adec5aba35080cc4bff0cb3
Adrian <adrian.deanda@runahr.com>
2018-12-02 00:11:26 -0600

    Code cleanup for first puzzle

--- Day2/main.py
+++ Day2/main.py
@@ -5,19 +5,13 @@ with open("input.txt") as n:
 
 # Solution for first puzzle
 
-def checksum(ids):
-    twos = 0
-    threes = 0
+def checksum(ids, twos: int = 0, threes: int = 0):
     for n in range (0, len(ids)):
         dict = {i:ids[n].count(i) for i in ids[n]}
-        # print(2 in dict.values())
-        # print (dict)
         if 2 in dict.values():
             twos += 1
-
         if 3 in dict.values():
             threes += 1
-        # threes = lambda "3" in dict.values() : += 1
     return twos * threes
 
 assert checksum([
