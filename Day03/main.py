b68a09e3a7e52e6a8942092e46344b74932837ba
Adrian De Anda <adrian.deanda@outlook.com>
2018-12-03 01:32:06 -0600

    Solution for Day 03 Puzzle 2

--- Day03/main.py
+++ Day03/main.py
@@ -6,7 +6,7 @@ with open("input.txt") as n:
 
 regex = "#([0-9]+) @ ([0-9]+),([0-9]+): ([0-9]+)x([0-9]+)"
 
-# Solution for firts puzzle
+# Solution for first puzzle
 
 
 def split_input(list):
@@ -14,29 +14,54 @@ def split_input(list):
     return split
 
 
-def count_overlapped(new_matrix, height, width, overlapped: int = 0):
-    for i in range(0, height):
-        for j in range(0, width):
-            if new_matrix[j][i] == -1:
-                overlapped += 1
-    return overlapped
-
-
-def fill_matrix(data, height: int = 1000, width: int = 1000):
-    matrix = np.zeros((height, width))
+def create_matrix(data, height, width):
+    new_matrix = np.zeros((height, width))
     for i in range(len(data)):
         rectangle = split_input(data[i])
         for x in range((rectangle[1]), (rectangle[1] + rectangle[3])):
             for y in range((rectangle[2]), (rectangle[2] + rectangle[4])):
-                if matrix[y][x] == 0:
-                    matrix[y][x] = rectangle[0]
-                elif matrix[y][x] != 0:
-                    matrix[y][x] = -1
-    return count_overlapped(matrix, height, width)
+                if new_matrix[y][x] == 0:
+                    new_matrix[y][x] = rectangle[0]
+                elif new_matrix[y][x] != 0:
+                    new_matrix[y][x] = -1
+    return new_matrix
+
+
+def count_overlapped(
+    data, height: int = 1000, width: int = 1000, overlapped: int = 0
+):
+    matrix = create_matrix(data, height, width)
+    for i in range(0, height):
+        for j in range(0, width):
+            if matrix[i][j] == -1:
+                overlapped += 1
+    return overlapped
 
 
 assert (
-    fill_matrix(["#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"], 8, 8) == 4
+    count_overlapped(["#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"], 8, 8)
+    == 4
 )
 
-print(f"Number of square inches in other claims: {fill_matrix(input)}")
+
+print(f"Number of square inches in other claims: {count_overlapped(input)}")
+
+# Solution for second puzzle
+
+
+def clean_claim(data, height: int = 1000, width: int = 1000):
+    claim_matrix = create_matrix(data, height, width)
+    unique, counts = np.unique(claim_matrix, return_counts=True)
+    values = dict(zip(unique, counts))
+    for i in range(len(data)):
+        rectangle = split_input(data[i])
+        square_inches = rectangle[3] * rectangle[4]
+        if rectangle[0] in values and values[rectangle[0]] == square_inches:
+            return rectangle[0]
+
+
+assert (
+    clean_claim(["#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"], 8, 8) == 3
+)
+
+print(f"Claim ID that doesn't overlap: {clean_claim(input)}")
